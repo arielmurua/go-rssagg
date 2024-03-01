c1549eb70df8ff7edccd5093d72834fb3ba3f8f1
arielmurua <arielmur33@gmail.com>
2024-03-01 03:31:48 -0300

    server

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("testing...")
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT not found")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	fmt.Printf("Server running on port: %v", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
